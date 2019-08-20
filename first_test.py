26b50b32791108bfb2026e1095ae7946d983f688
Zhang Shuai <520zhangshuai@gmail.com>
2019-08-20 11:04:57 +0800

    [Shuai] add first pytest

---
+++ first_test.py
@@ -0,0 +1,58 @@
+import os
+import pytest
+
+from appium import webdriver
+from helpers import take_screenhot_and_logcat
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+APPIUM_LOCAL_HOST_URL = 'http://localhost:4723/wd/hub'
+
+
+class TestSimpleAndroid():
+    @pytest.fixture(scope="function")
+    def driver(self, request, device_logger):
+        desired_caps = {
+            'platformName': 'Android',
+            'platformVersion': '8.1.0',
+            'deviceName': 'QHAP8YNK8Q',
+            'app': PATH('/Users/shuaiz/Downloads/hmipatent-debug.apk')
+        }
+        calling_request = request._pyfuncitem.name
+        driver = webdriver.Remote(APPIUM_LOCAL_HOST_URL, desired_caps)
+
+        def fin():
+            take_screenhot_and_logcat(driver, device_logger, calling_request)
+            driver.quit()
+
+        request.addfinalizer(fin)
+        return driver  # provide the fixture value
+
+    def test_find_elements(self, driver):
+        el = driver.find_elements_by_class_name('android.widget.Button')
+        el[1].click()
+        el = driver.find_elements_by_class_name('android.widget.Button')
+        el[1].click()
+        el = driver.find_elements_by_class_name('android.widget.Button')
+        el[1].click()
+        # el = driver.find_element_by_accessibility_id('Arcs')
+        # assert el is not None
+        # driver.back()
+
+        # el = driver.find_element_by_accessibility_id("App")
+        # assert el is not None
+
+        # els = driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
+        # assert len(els) >= 12
+        # driver.find_element_by_android_uiautomator('text("API Demos")')
+
+    # def test_simple_actions(self, driver):
+    #     el = driver.find_element_by_accessibility_id('Graphics')
+    #     el.click()
+
+    #     el = driver.find_element_by_accessibility_id('Arcs')
+    #     el.click()
+
+    #     driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
